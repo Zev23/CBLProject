345cc862cfdb15e4101bb1956a0034c4e6fbc97e
Zev Teh <zev.teh@gmail.com>
2016-06-19 15:21:15 +0800

    Initial Commit

---
+++ CBLProject/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  CBLProject
+//
+//  Created by Zev on 19/06/2016.
+//  Copyright © 2016 Zev23.com. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
